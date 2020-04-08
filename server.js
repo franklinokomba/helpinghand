d03a9429a188d7f0097a187ba832997d597bb8e8
beverly <beverly99@hotmail.fr>
2020-04-07 22:12:01 -0700

    routes added

--- server.js
+++ server.js
@@ -1,8 +1,29 @@
 const express = require("express");
 const morgan = require("morgan");
+const bodyParser = require('body-parser')
+
 const app = express();
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true })) //form
 app.use(morgan("dev"));
+
+//ROUTES
+app.get("/", function(req,res){
+    res.send("Welcome");    
+})
+
+app.post("/login", (req,res)=>{
+    // Check that user is in db
+    // check that password is correct
+    //generate token and send to user
+    const {name, email, password} = req.body;
+    const response = `You sent name = ${name} , email=${email} , password=${password}`;
+    res.send(response);
+})
+
 const port = process.env.PORT || 6112;
 app.listen(port, function () {
 	console.log(`Server is listening on port: ${port}`);
 });
+
